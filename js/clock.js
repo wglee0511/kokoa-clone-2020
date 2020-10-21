75959d68c08e09768dd7620a4162cda87446d5b8
wglee0511 <ranazang@naver.com>
2020-10-21 22:09:59 +0900

    Update time js

---
+++ js/clock.js
@@ -0,0 +1,21 @@
+const timeContainer = document.querySelector(".js-clock");
+const currentTime = timeContainer.querySelector("span");
+
+function getTime(){
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    currentTime.innerText
+    =`${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+}
+
+
+function init(){
+    getTime();
+    setInterval(getTime,1000);
+    
+}
+
+init();
